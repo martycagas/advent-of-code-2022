c8838173a2bbefe96c3e33d8876a4ec8a7c8d169
Marty Cagas <martycagas@users.noreply.github.com>
2022-12-05 22:22:47 +0100

    Fixed using wrong exception in import try blocks

--- 1/day_1.py
+++ 1/day_1.py
@@ -1,11 +1,11 @@
 try:
     from part1.solution import solution as solution_1
     solution_1('input.txt')
-except FileNotFoundError:
+except ImportError:
     pass
 
 try:
     from part2.solution import solution as solution_2
     solution_2('input.txt')
-except FileNotFoundError:
+except ImportError:
     pass
