b5d8f345931f1da032c2c462734841af6e018346
Marty Cagas <martycagas@users.noreply.github.com>
2022-12-11 21:58:52 +0100

    Added a more elegant solution for the day 2
    I do these challenges to get better at Python. The old solution wasn't clever in any way. I didn't like it.

--- 2/solver.py
+++ 2/solver.py
@@ -1,112 +1,74 @@
+import sys
+
+
 class Solver:
+    # Tuples of available moves
+    ENEMY_MOVES = ('A', 'B', 'C')
+    PLAYER_MOVES = ('X', 'Y', 'Z')
+    # Translation dictionaries
+    ENEMY_MOVES_MAP = {'A': 0, 'B': 1, 'C': 2}
+    PLAYER_MOVES_MAP = {'X': 0, 'Y': 1, 'Z': 2}
+    # Scoring dictionary, with values defined by the assignment
+    MOVE_SCORES = (
+        1,  # rock
+        2,  # paper
+        3  # scisors
+    )
+    # Result scores, values defined by the assignment
+    RESULT_SCORES = (
+        3,  # draw
+        6,  # win
+        0  # loss
+    )
+
     def __init__(self, file: str) -> None:
         with open(file, 'r') as f:
             # Split the input to lines
             self.input = f.read().strip()
 
     @staticmethod
-    def eval_move(enemy: str, player: str) -> int:
-        combined = enemy + player
-        match combined:
-            case 'AX':
-                # Rock and rock
-                return 3
-            case 'AY':
-                # Rock and paper
-                return 6
-            case 'AZ':
-                # Rock and scisors
-                return 0
-            case 'BX':
-                # Paper and rock
-                return 0
-            case 'BY':
-                # Paper and paper
-                return 3
-            case 'BZ':
-                # Paper and scisors
-                return 6
-            case 'CX':
-                # Scisors and rock
-                return 6
-            case 'CY':
-                # Scisors and paper
-                return 0
-            case 'CZ':
-                # Scisors and scisors
-                return 3
-            case _:
-                print(
-                    'Make sure the left argumement contains only [ABC] and right one contains only [XYZ]')
-                raise ValueError
+    def eval_move(enemy_move: str, player_move: str) -> int:
+        # The number representation of an enemy move
+        index = Solver.ENEMY_MOVES_MAP[enemy_move]
+        # The number representation of a player move
+        PLAYER_INDEX = Solver.PLAYER_MOVES_MAP[player_move]
+        distance = 0
+        # Keep incrementing the index until you land on the player move
+        # You've now gained the cyclic distance to the player move
+        while index != PLAYER_INDEX and distance < 3:
+            distance += 1
+            index = (index + 1) % len(Solver.RESULT_SCORES)
+        # From the cyclic distance, we can now determine the result (and its score)
+        return Solver.RESULT_SCORES[distance]
 
     @staticmethod
     def decode_move(enemy_move: str, desired_outcome: str) -> str:
-        combined = enemy_move + desired_outcome
-        # Enemy move:      A = X = rock
-        #                  B = Y = paper
-        #                  C = Z = Scisors
-        # Desired outcome: X = lose
-        #                  Y = draw
-        #                  Z = win
-        match combined:
-            case 'AX':
-                # Lose against a rock
-                return 'Z'
-            case 'AY':
-                # Draw against a rock
-                return 'X'
-            case 'AZ':
-                # Win against a rock
-                return 'Y'
-            case 'BX':
-                # Lose against a paper
-                return 'X'
-            case 'BY':
-                # Draw against a paper
-                return 'Y'
-            case 'BZ':
-                # Win against a paper
-                return 'Z'
-            case 'CX':
-                # Lose against a scisors
-                return 'Y'
-            case 'CY':
-                # Draw against a scisors
-                return 'Z'
-            case 'CZ':
-                # Win against a scisors
-                return 'X'
-            case _:
-                print(
-                    'Make sure the left argumement contains only [ABC] and right one contains only [XYZ]')
-                raise ValueError
+        # The number representation of an enemy move
+        ENEMY_MOVE_INDEX = Solver.ENEMY_MOVES_MAP[enemy_move]
+        # The map of desired moves
+        OUTCOME_MAP = {'X': 2, 'Y': 0, 'Z': 1}
+        # Translate an enemy move to an index and use it as a startpoint
+        # Increase that index depending on the desired outcome
+        # Select a player move from the PLAYER_MOVES tuple, but don't forget to loop it back to start
+        return Solver.PLAYER_MOVES[(ENEMY_MOVE_INDEX + OUTCOME_MAP[desired_outcome]) % len(Solver.PLAYER_MOVES)]
 
     def solver_part_one(self) -> int:
         total_score = 0
-        # A = X = rock
-        # B = Y = paper
-        # C = Z = Scisors
-        move_values = {'X': 1, 'Y': 2, 'Z': 3}
         # Read the file contents, strip them, split them to lines and iterate over them
         for line in self.input.split('\n'):
             enemy_move, player_move = line.split(' ')
-            total_score += move_values[player_move]
+            total_score += Solver.MOVE_SCORES[Solver.PLAYER_MOVES_MAP[player_move]]
             total_score += Solver.eval_move(enemy_move, player_move)
         # Print out the result
         return total_score
 
     def solver_part_two(self) -> int:
         total_score = 0
-        # A = X = rock
-        # B = Y = paper
-        # C = Z = Scisors
-        move_values = {'X': 1, 'Y': 2, 'Z': 3}
         # Read the file contents, strip them, split them to lines and iterate over them
         for line in self.input.split('\n'):
             enemy_move, player_move = line.split(' ')
             player_move = Solver.decode_move(enemy_move, player_move)
-            total_score += move_values[player_move]
+            total_score += Solver.MOVE_SCORES[Solver.PLAYER_MOVES_MAP[player_move]]
             total_score += Solver.eval_move(enemy_move, player_move)
         # Print out the result
         return total_score
@@ -118,7 +80,6 @@ class Solver:
 
 if __name__ == '__main__':
     import argparse
-
     parser = argparse.ArgumentParser(
         description='Advent of Code 2022 day one puzzle solver')
     parser.add_argument('-i', '--input', type=str,
