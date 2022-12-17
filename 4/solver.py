036dad9b55307f4aa134d8aca65fd7cd7faa6db6
Marty Cagas <martycagas@users.noreply.github.com>
2022-12-17 19:17:50 +0100

    Solved day 4, part 1

--- 4/solver.py
+++ 4/solver.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Solver:
     class LoopBreakException(Exception):
         pass
@@ -5,9 +8,18 @@ class Solver:
     def __init__(self, file: str) -> None:
         with open(file, 'r') as f:
             self.input = f.read().strip()
+        self.boundaries = []
+        for line in self.input.split('\n'):
+            self.boundaries.append([int(i) for i in re.split(',|-', line)])
 
     def solver_part_one(self) -> int:
-        pass
+        fully_contained = 0
+        for boundary in self.boundaries:
+            first = set(range(boundary[0], boundary[1] + 1))
+            second = set(range(boundary[2], boundary[3] + 1))
+            if first.issubset(second) or second.issubset(first):
+                fully_contained += 1
+        return fully_contained
 
     def solver_part_two(self) -> int:
         pass
