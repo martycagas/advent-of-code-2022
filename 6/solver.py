3c6e0864c0319b0d187c20576da58466e880d616
Marty Cagas <martycagas@users.noreply.github.com>
2023-01-01 19:01:08 +0100

    Solved day 6, part 1

--- 6/solver.py
+++ 6/solver.py
@@ -1,10 +1,46 @@
+from collections import deque
+
+
 class Solver:
+    class CharBuffer:
+        def __init__(self, max_len: int = 4) -> None:
+            self.buffer: deque[str] = deque('', max_len)
+
+        def __str__(self) -> str:
+            ret_str = ''
+            for char in self.buffer:
+                ret_str += char
+            return ret_str
+
+        def push_char(self, char: str) -> None:
+            if len(char) == 1:
+                self.buffer.append(char)
+            else:
+                raise ValueError
+
+        def are_all_chars_unique(self) -> bool:
+            buffer_as_str = str(self)
+            for i in range(len(buffer_as_str)):
+                comp_string = buffer_as_str[:i] + buffer_as_str[i + 1:]
+                if buffer_as_str[i] in comp_string:
+                    return False
+            else:
+                return True
+
     def __init__(self, file: str) -> None:
         with open(file, 'r') as f:
             self.input: str = f.read().strip()
 
-    def solver_part_one(self) -> int:
-        pass
+    def solver_part_one(self, buffer_len: int = 4) -> int:
+        counter: int = buffer_len - 1
+        buffer: Solver.CharBuffer = Solver.CharBuffer(buffer_len)
+        for char in self.input[:buffer_len - 1]:
+            buffer.push_char(char)
+        for char in self.input[buffer_len - 1:]:
+            buffer.push_char(char)
+            counter += 1
+            if buffer.are_all_chars_unique():
+                return counter
 
     def solver_part_two(self) -> int:
         pass
