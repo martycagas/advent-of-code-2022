04c0d977ea77733d1a650e58ee28da7e8a1d9493
Marty Cagas <martycagas@users.noreply.github.com>
2023-01-01 03:08:15 +0100

    Added day 6 solver skeleton and inputs

---
+++ 6/solver.py
@@ -0,0 +1,23 @@
+class Solver:
+    def __init__(self, file: str) -> None:
+        with open(file, 'r') as f:
+            self.input = f.read().strip()
+
+    def solver_part_one(self) -> int:
+        pass
+
+    def solver_part_two(self) -> int:
+        pass
+
+    def solve(self) -> None:
+        print(self.solver_part_one())
+        print(self.solver_part_two())
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, required=True,)
+    args = parser.parse_args()
+    solver = Solver(args.input)
+    solver.solve()
