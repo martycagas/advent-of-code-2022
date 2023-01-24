a71e4c7c79be08bd3a73b2862a4cedeeb8eb9985
Marty Cagas <martycagas@users.noreply.github.com>
2023-01-25 00:49:43 +0100

    Moved nested classes outside of the containing class

--- 5/solver.py
+++ 5/solver.py
@@ -1,57 +1,58 @@
 import re
 
 
-class Solver:
-    class CargoDeck:
-        def __init__(self, box_map_string: str) -> None:
-            self.cargo: dict[int, list[str]] = {}
-            piles: list[str] = box_map_string.splitlines()
-            for column_index in re.split('\s+', piles[-1].strip()):
-                self.cargo[int(column_index)] = []
-            colum_count: int = len(self.cargo)
-            for line in piles[:-1]:
-                for i in range(colum_count):
-                    try:
-                        self.cargo[i + 1].append(line[1 + (i * 4)])
-                    except IndexError:
-                        pass
-            for key in self.cargo.keys():
-                self.cargo[key] = [
-                    item for item in reversed(self.cargo[key]) if item != ' '
-                ]
-
-        def __str__(self) -> str:
-            output: str = ''
-            for key in self.cargo.keys():
-                output += '{}: '.format(key)
-                for item in self.cargo[key]:
-                    output += '[{}] '.format(item)
-                output = output.strip() + '\n'
-            return output.strip()
-
-        def move(self, how_many: int, where_from: int, where_to: int) -> None:
-            for _ in range(how_many):
-                self.cargo[where_to].append(self.cargo[where_from].pop())
-
-        def move_multiple(self, how_many: int, where_from: int, where_to: int) -> None:
-            tmp_list: list = []
-            for _ in range(how_many):
+class CargoDeck:
+    def __init__(self, box_map_string: str) -> None:
+        self.cargo: dict[int, list[str]] = {}
+        piles: list[str] = box_map_string.splitlines()
+        for column_index in re.split('\s+', piles[-1].strip()):
+            self.cargo[int(column_index)] = []
+        colum_count: int = len(self.cargo)
+        for line in piles[:-1]:
+            for i in range(colum_count):
                 try:
-                    tmp_list.append(self.cargo[where_from].pop())
-                except IndexError:
-                    break
-            for _ in range(len(tmp_list)):
-                self.cargo[where_to].append(tmp_list.pop())
-
-        def get_top_row(self) -> str:
-            accumulator_string: str = ''
-            for column in self.cargo.values():
-                try:
-                    accumulator_string += column[-1]
+                    self.cargo[i + 1].append(line[1 + (i * 4)])
                 except IndexError:
                     pass
-            return accumulator_string
+        for key in self.cargo.keys():
+            self.cargo[key] = [
+                item for item in reversed(self.cargo[key]) if item != ' '
+            ]
 
+    def __str__(self) -> str:
+        output: str = ''
+        for key in self.cargo.keys():
+            output += '{}: '.format(key)
+            for item in self.cargo[key]:
+                output += '[{}] '.format(item)
+            output = output.strip() + '\n'
+        return output.strip()
+
+    def move(self, how_many: int, where_from: int, where_to: int) -> None:
+        for _ in range(how_many):
+            self.cargo[where_to].append(self.cargo[where_from].pop())
+
+    def move_multiple(self, how_many: int, where_from: int, where_to: int) -> None:
+        tmp_list: list = []
+        for _ in range(how_many):
+            try:
+                tmp_list.append(self.cargo[where_from].pop())
+            except IndexError:
+                break
+        for _ in range(len(tmp_list)):
+            self.cargo[where_to].append(tmp_list.pop())
+
+    def get_top_row(self) -> str:
+        accumulator_string: str = ''
+        for column in self.cargo.values():
+            try:
+                accumulator_string += column[-1]
+            except IndexError:
+                pass
+        return accumulator_string
+
+
+class Solver:
     def __init__(self, file: str) -> None:
         with open(file, 'r') as f:
             input = re.split('\n\n', f.read().strip())
@@ -59,7 +60,7 @@ class Solver:
             self.moves_input: list[str] = input[1].splitlines()
 
     def solve_part_one(self) -> str:
-        cargo_deck: Solver.CargoDeck = Solver.CargoDeck(self.cargo_input)
+        cargo_deck: CargoDeck = CargoDeck(self.cargo_input)
         for line in self.moves_input:
             operands = line.strip().split(' ')
             cargo_deck.move(int(operands[1]), int(
@@ -67,7 +68,7 @@ class Solver:
         return cargo_deck.get_top_row()
 
     def solve_part_two(self) -> str:
-        cargo_deck: Solver.CargoDeck = Solver.CargoDeck(self.cargo_input)
+        cargo_deck: CargoDeck = CargoDeck(self.cargo_input)
         for line in self.moves_input:
             operands = line.strip().split(' ')
             cargo_deck.move_multiple(
--- 6/solver.py
+++ 6/solver.py
@@ -1,39 +1,40 @@
 from collections import deque
 
 
+class CharBuffer:
+    def __init__(self, max_len: int) -> None:
+        self.buffer: deque[str] = deque('', max_len)
+
+    def __str__(self) -> str:
+        ret_str = ''
+        for char in self.buffer:
+            ret_str += char
+        return ret_str
+
+    def push_char(self, char: str) -> None:
+        if len(char) == 1:
+            self.buffer.append(char)
+        else:
+            raise ValueError
+
+    def are_all_chars_unique(self) -> bool:
+        buffer_as_str = str(self)
+        for i in range(len(buffer_as_str)):
+            comp_string = buffer_as_str[:i] + buffer_as_str[i + 1:]
+            if buffer_as_str[i] in comp_string:
+                return False
+        else:
+            return True
+
+
 class Solver:
-    class CharBuffer:
-        def __init__(self, max_len: int) -> None:
-            self.buffer: deque[str] = deque('', max_len)
-
-        def __str__(self) -> str:
-            ret_str = ''
-            for char in self.buffer:
-                ret_str += char
-            return ret_str
-
-        def push_char(self, char: str) -> None:
-            if len(char) == 1:
-                self.buffer.append(char)
-            else:
-                raise ValueError
-
-        def are_all_chars_unique(self) -> bool:
-            buffer_as_str = str(self)
-            for i in range(len(buffer_as_str)):
-                comp_string = buffer_as_str[:i] + buffer_as_str[i + 1:]
-                if buffer_as_str[i] in comp_string:
-                    return False
-            else:
-                return True
-
     def __init__(self, file: str) -> None:
         with open(file, 'r') as f:
             self.input: str = f.read().strip()
 
     def solve_both(self, buffer_len: int) -> int:
         counter: int = buffer_len - 1
-        buffer: Solver.CharBuffer = Solver.CharBuffer(buffer_len)
+        buffer: CharBuffer = CharBuffer(buffer_len)
         for char in self.input[:buffer_len - 1]:
             buffer.push_char(char)
         for char in self.input[buffer_len - 1:]:
