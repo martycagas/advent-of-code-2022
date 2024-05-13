2d9fa5b6edfa97df5321c210bc2cb8c0bf8937bb
Marty Cagas <martycagas@users.noreply.github.com>
2024-05-14 01:08:12 +0200

    Pushing a non-functional version for sync between devices

--- 10/solver.py
+++ 10/solver.py
@@ -1,13 +1,55 @@
 from pathlib import Path
+from enum import Enum, auto
+
+
+class ProcState(Enum):
+    READY = auto()
+    ADDITION = auto()
 
 
 class Solver:
     def __init__(self, file: Path) -> None:
         with file.open() as f:
-            self.input: str = f.read().strip()
+            self.input: list[str] = f.read().splitlines()
 
     def solve_part_one(self) -> int:
-        pass
+        cycle = 0
+        index = 0
+        register = 1
+        operand: int | None = None
+        accumulator = 0
+        state = ProcState.READY
+
+        while True:
+            cycle += 1
+            if state == ProcState.READY:
+                try:
+                    line = self.input[index]
+                    index += 1
+                except IndexError:
+                    break
+
+                match line.split():
+                    case ["noop"]:
+                        state = ProcState.READY
+                    case ["addx", op]:
+                        operand = int(op)
+                        state = ProcState.ADDITION
+                    case _:
+                        raise ValueError(f"Unable to parse a line: {line}!")
+            elif state == ProcState.ADDITION:
+                if operand:
+                    register += operand
+                    operand = None
+                else:
+                    raise RuntimeError(f"Tried to add NULL operand, cycle={cycle}!")
+                state = ProcState.READY
+
+            if cycle == 20:
+                accumulator += register * cycle
+            elif (cycle - 20) % 40 == 0:
+                accumulator += register * cycle
+        return accumulator
 
     def solve_part_two(self) -> int:
         pass
